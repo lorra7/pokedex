251a4813653798c57f87a600085bf92cf218e1b7
Laura-baratier <161334955+lorra7@users.noreply.github.com>
2024-04-07 21:57:47 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,12 @@ function App() {
   return (
     <div>
       <PokemonCard pokemon={pokemonList[0]} />
-       <PokemonCard pokemon={pokemonList[1]} />
-     
-
     </div>
   );
 }
 
 export default App;
 
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc : "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    
-  },
-  {
-    name: "mew",
-  },
-];
 
 
 
