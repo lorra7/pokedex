6c7daed95d65be42ec82afd27a57dab9d22a4080
Laura-baratier <161334955+lorra7@users.noreply.github.com>
2024-04-07 21:51:44 +0200

    Update PokemonCard.jsx

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,51 @@
-function PokemonCard({ pokemon }) {
+import PropTypes from "prop-types";
 
-    return (
-        <figure>{pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt={"pokemon.name"} />) : (<p>???</p>)}
-            <figcaption>{pokemon.name}</figcaption>
-        </figure>
+//Ajout de données : listes pokémon
+const pokemonList = [
+  {
+    name: "Bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
 
-    );
+  {
+    name: "Dracofeu",
+    imgSrc:
+      "https://www.pokepedia.fr/images/thumb/1/17/Dracaufeu-RFVF.png/800px-Dracaufeu-RFVF.png",
+  }
+];
+PokemonCard.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imgSrc: PropTypes.string,
+
+  }).isRequired,
 }
 
+function PokemonCard() {
+  const pokemonTrainer = "Laura";
+  const pokemon = pokemonList[2];
+
+  return (
+    <>
+      <h3>
+        Hello <strong>{pokemonTrainer}</strong> trainer !
+
+      </h3>
+      <figure>
+        {pokemon.imgSrc != null ? (
+          <img src={pokemon.imgSrc} alt={pokemon.name} />
+        ) : (
+          <p>???</p>
+        )}
+
+        <figcaption>{pokemon.name}</figcaption>
+      </figure>
+    </>
+  );
+}
 
 export default PokemonCard;
